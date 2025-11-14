4a81ed1ccd495ba5f6db42a34b9be94d0f873ee1
Quantam.PX <190332698+QuantamPX@users.noreply.github.com>
2025-11-13 19:20:13 -0600

    human choice function and global variables

--- main.js
+++ main.js
@@ -4,8 +4,24 @@ function get_computer_choice(){
     const choices = ['rock', 'paper', 'scissors'];
     const random_index = Math.floor(Math.random() * choices.length);
     const random_element = choices[random_index];
-    console.log(random_element);
+    //console.log(random_element);
 }
 
 // get_computer_choice();
+
+function get_human_choice(){
+    const choices = ['rock', 'paper', 'scissors'];
+    let human_choice = window.prompt("Rock, Paper, or Scissors? ");
+    return human_choice;
+}
+
+//get_human_choice();
+
+// declare player score variables (global)
+
+let human_score = 0;
+let computer_score = 0;
+
+
+
 
